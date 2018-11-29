c8fcdf42c4202d9f9ce2eded6098b0ea349f222b
Torii <Takashi.Torii@emc.com>
2018-11-29 12:12:50 +0900

    add tf files

---
+++ vars.tf
@@ -0,0 +1,39 @@
+variable "viuser" {}
+
+variable "vipassword" {}
+
+variable "viserver" {}
+
+variable "datacenter" {
+  default = ""
+}
+
+// default VM name in vSphere
+variable "vmname" {
+  default = ""
+}
+
+// default Resource Pool
+variable "vmrp" {
+  default = ""
+}
+
+// default VM domain for guest customization
+variable "vmdomain" {
+  default = ""
+}
+
+// default datastore to deploy vmdk
+variable "vmdatastore" {
+  default = ""
+}
+
+// default VM Template
+variable "vmtemp" {
+  default = ""
+}
+
+// map of the VM Network (vmdomain = "vmnetlabel")
+variable "vmnetlabel" {
+  default = ""
+}
